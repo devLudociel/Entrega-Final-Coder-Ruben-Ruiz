9fa21422990ba012f20ed35ac82fec0f9e4bb9de
Ludociel <rubenjruiz1441@gmail.com>
2023-05-10 14:16:37 -0400

    Entrega Final de coder, falta agregar unas paginas y funcionalidades, asi como dar mejores estilos UI a la pagina

---
+++ js/inicioSesion.js
@@ -0,0 +1,53 @@
+const verCredenciales = document.getElementById("verCredenciales")
+const btnLogin = document.getElementById("login")
+
+
+verCredenciales.addEventListener("click", credenciales);
+function credenciales() {
+
+    Swal.fire({
+        title: 'Ingesa con:',
+        icon: 'info',
+        iconColor: '#ecaeae',
+        html: ` Nombre: profesor
+                <br>
+                password: coder`,
+        focusConfirm: false,
+        confirmButtonText: 'Okey',
+        confirmButtonColor: '#ecaeae',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location = ""
+        }
+    })
+};
+
+btnLogin.addEventListener("click", login);
+function login(){
+    document.querySelector('form')
+    .addEventListener('submit', (e) =>{
+        e.preventDefault()
+        const user = document.getElementById("user").value,
+        password = document.getElementById("password").value;
+        if (user == "profesor" && password == "coder"){
+            window.location = "./productos.html"
+        }
+        else {
+            Swal.fire({
+                title: 'Debes leer las Credenciales:',
+                icon: 'info',
+                iconColor: '#ecaeae',
+                html: ` Presiona sobre el boton
+                        <br>
+                        ver credenciales`,
+                focusConfirm: false,
+                confirmButtonText: 'Okey',
+                confirmButtonColor: '#ecaeae',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location = ""
+                }
+            });
+        };
+    });
+};
